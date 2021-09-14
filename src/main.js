19d134642ce0b48b4a8b7f0036c8fb309a9cfde6
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-09-14 20:36:12 -0300

    bug fix

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import './plugins/axios'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './plugins/bootstrap-vue'
